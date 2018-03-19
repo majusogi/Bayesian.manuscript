3a46c910f3a7b386dbb06899bcc9697cf6846666
Juliana Soto-Giron <dendroapsis@gmail.com>
2018-03-19 10:15:45 -0400

    Update Gene.under.recom.bash

--- Scripts/Gene.under.recom.bash
+++ Scripts/Gene.under.recom.bash
@@ -4,3 +4,12 @@
 
 To mask conserved seq genes in the genome: 
 
+module load bedtools/2.25 
+bedtools maskfasta -fi mauve.2537562117.fa -bed to.mask.txt -fo masked.2537562117.fa -mc X
+
+sed 's/X*//g' maksed.110.genome.CUT2.fa > ensayo.fa
+
+tr -d '\n' < ensayo.fa > ensayo2.fa
+
+input file: to.mask.txt
+
